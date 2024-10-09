50ebc61bf07f37711c88e456ad44bed28848952b
Pargusz <147164280+Pargusz@users.noreply.github.com>
2024-10-10 00:12:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-# Ana sayfa
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# WebRTC için sinyal alışverişi
+# WebRTC signaling
 @socketio.on('offer')
-def handle_offer(data):
-    socketio.emit('offer', data, broadcast=True)
+def handle_offer(offer):
+    socketio.emit('offer', offer, broadcast=True)
 
 @socketio.on('answer')
-def handle_answer(data):
-    socketio.emit('answer', data, broadcast=True)
+def handle_answer(answer):
+    socketio.emit('answer', answer, broadcast=True)
 
 @socketio.on('ice-candidate')
-def handle_ice_candidate(data):
-    socketio.emit('ice-candidate', data, broadcast=True)
+def handle_ice_candidate(candidate):
+    socketio.emit('ice-candidate', candidate, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True)
